144b90802c355e0271e1346e3b4ac46cbd8832b2
tfkachmad <taufik.achmad.septian@gmail.com>
2023-02-24 06:22:03 +0700

    Update case study 2 data cleaning query

--- Case-Study-2-Pizza-Runner/SQL-files/Case-Study-2-Pizza-Runner-Data-Cleaning.sql
+++ Case-Study-2-Pizza-Runner/SQL-files/Case-Study-2-Pizza-Runner-Data-Cleaning.sql
@@ -1,104 +1,172 @@
-USE EightWeekSQLChallenge;
+SET search_path = pizza_runner;
+
+-- Cleaning customer_orders table
+DROP TABLE IF EXISTS customer_orders_clean;
+
+CREATE TABLE customer_orders_clean (
+    unique_id INTEGER,
+    order_id INTEGER,
+    customer_id INTEGER,
+    pizza_id INTEGER,
+    exclusions VARCHAR(4),
+    extras VARCHAR(4),
+    order_time TIMESTAMP
+);
+
+INSERT INTO customer_orders_clean (unique_id, order_id, customer_id, pizza_id, exclusions, extras, order_time)
+SELECT
+    ROW_NUMBER() OVER (ORDER BY order_id),
+    order_id,
+    customer_id,
+    pizza_id,
+    CASE WHEN exclusions IN ('', 'null', NULL) THEN
+        NULL
+    ELSE
+        exclusions
+    END AS exclusions,
+    CASE WHEN extras IN ('', 'null', NULL) THEN
+        NULL
+    ELSE
+        extras
+    END AS extras,
+    order_time
+FROM
+    customer_orders;
+
 --
-/*
-	========	DATA CLEANING	========
-*/
+-- Cleaning runner_orders table
+DROP TABLE IF EXISTS runner_orders_clean;
+
+CREATE TABLE runner_orders_clean (
+    order_id INTEGER,
+    runner_id INTEGER,
+    pickup_time TIMESTAMP,
+    distance FLOAT,
+    duration INTEGER,
+    cancellation VARCHAR(23)
+);
+
+INSERT INTO runner_orders_clean (order_id, runner_id, pickup_time, distance, duration, cancellation)
+SELECT
+    order_id,
+    runner_id,
+    CASE WHEN pickup_time IN ('', 'null', NULL) THEN
+        NULL
+    ELSE
+        pickup_time::TIMESTAMP
+    END AS pickup_time2,
+    CASE WHEN distance IN ('', 'null', NULL) THEN
+        NULL
+    ELSE
+        TRIM(REPLACE(distance, 'km', ''))::FLOAT
+    END AS distance2,
+    CASE WHEN duration IN ('', 'null', NULL) THEN
+        NULL
+    ELSE
+        TRIM(REPLACE(REPLACE(REPLACE(duration, 'minutes', ''), 'minute', ''), 'mins', ''))::INTEGER
+    END AS duration2, CASE WHEN cancellation IN ('', 'null', NULL) THEN
+        NULL
+    ELSE
+        cancellation
+    END AS cancellation FROM runner_orders;
+
+-- Creating pizza_recipes_long table
+DROP TABLE IF EXISTS pizza_recipes_long;
+
+CREATE TABLE pizza_recipes_long (
+    pizza_id INTEGER,
+    topping_id INTEGER
+);
+
+INSERT INTO pizza_recipes_long (pizza_id, topping_id)
+SELECT
+    pizza_id,
+    UNNEST(STRING_TO_ARRAY(toppings, ','))::INTEGER AS topping_id
+FROM
+    pizza_runner.pizza_recipes;
+SET search_path = pizza_runner;
+
+-- Cleaning customer_orders table
+DROP TABLE IF EXISTS customer_orders_clean;
+
+CREATE TABLE customer_orders_clean (
+    unique_id INTEGER,
+    order_id INTEGER,
+    customer_id INTEGER,
+    pizza_id INTEGER,
+    exclusions VARCHAR(4),
+    extras VARCHAR(4),
+    order_time TIMESTAMP
+);
+
+INSERT INTO customer_orders_clean (unique_id, order_id, customer_id, pizza_id, exclusions, extras, order_time)
+SELECT
+    ROW_NUMBER() OVER (ORDER BY order_id),
+    order_id,
+    customer_id,
+    pizza_id,
+    CASE WHEN exclusions IN ('', 'null', NULL) THEN
+        NULL
+    ELSE
+        exclusions
+    END AS exclusions,
+    CASE WHEN extras IN ('', 'null', NULL) THEN
+        NULL
+    ELSE
+        extras
+    END AS extras,
+    order_time
+FROM
+    customer_orders;
+
 --
---	----------------------------------------------
---	Cleaning data in the customer_orders table
---	----------------------------------------------
-DROP TABLE
-IF EXISTS ##customer_orders_cleaned;
-	SELECT order_id
-		,customer_id
-		,pizza_id
-		-- Fixing inconsistent NULL in the exclusions column
-		,CASE
-			WHEN exclusions = ''
-				THEN NULL
-			WHEN exclusions = 'null'
-				THEN NULL
-			ELSE exclusions
-			END AS exclusions
-		-- Fixing inconsistent NULL in the extras column
-		,CASE
-			WHEN extras = ''
-				THEN NULL
-			WHEN extras = 'null'
-				THEN NULL
-			ELSE extras
-			END AS extras
-		,order_time
-		,ROW_NUMBER() OVER (
-			ORDER BY order_time
-			) AS order_row
-	INTO ##customer_orders_cleaned
-	FROM pizza_runner.customer_orders;
---
---	----------------------------------------------
---	Cleaning data in the runner_orders table
---	----------------------------------------------
-DROP TABLE
-IF EXISTS ##runner_orders_cleaned;
-	SELECT order_id
-		,runner_id
-		-- Fixing inconsistent NULL in the pickup_time column
-		,CASE
-			WHEN pickup_time = 'null'
-				THEN NULL
-			ELSE pickup_time
-			END AS pickup_time
-		-- Fixing inconsistent values and NULL in the distance column
-		,CASE
-			WHEN distance = 'null'
-				THEN NULL
-			ELSE Convert(FLOAT, Trim(Replace(distance, 'km', '')))
-			END AS distance
-		-- Fixing inconsistent values and NULL in the duration column
-		,CASE
-			WHEN duration = 'null'
-				THEN NULL
-			ELSE Convert(INT, Trim(Replace(Replace(Replace(duration, 'minutes', ''), 'minute', ''), 'mins', '')))
-			END AS duration
-		,CASE
-			WHEN cancellation = 'null'
-				THEN NULL
-			WHEN cancellation = ''
-				THEN NULL
-			WHEN cancellation IS NULL
-				THEN NULL
-			ELSE cancellation
-			END AS cancellation
-	INTO ##runner_orders_cleaned
-	FROM pizza_runner.runner_orders;
---
---	----------------------------------------------
---	Cleaning data in the pizza_recipes table
---	----------------------------------------------
-DROP TABLE
-IF EXISTS ##pizza_recipes_cleaned;
-	SELECT pizza_id
-		,TRIM(value) AS toppings
-	INTO ##pizza_recipes_cleaned
-	FROM pizza_runner.pizza_recipes
-	CROSS APPLY STRING_SPLIT(CONVERT(VARCHAR, toppings), ',');
---
--- ----------------------------------------------
--- Cleaning data in the pizza_names table
--- ----------------------------------------------
-DROP TABLE
-IF EXISTS ##pizza_names_cleaned;
-	SELECT pizza_id
-		,CONVERT(VARCHAR(20), pizza_name) AS pizza_name
-	INTO ##pizza_names_cleaned
-	FROM pizza_runner.pizza_names;
---
---	----------------------------------------------
---	Cleaning data in the pizza_toppings table
---	----------------------------------------------
-DROP TABLE
-IF EXISTS ##pizza_toppings_cleaned;
-	SELECT topping_id
-		,CONVERT(VARCHAR(20), topping_name) AS topping_name
-	INTO ##pizza_toppings_cleaned
-	FROM pizza_runner.pizza_toppings;
+-- Cleaning runner_orders table
+DROP TABLE IF EXISTS runner_orders_clean;
+
+CREATE TABLE runner_orders_clean (
+    order_id INTEGER,
+    runner_id INTEGER,
+    pickup_time TIMESTAMP,
+    distance FLOAT,
+    duration INTEGER,
+    cancellation VARCHAR(23)
+);
+
+INSERT INTO runner_orders_clean (order_id, runner_id, pickup_time, distance, duration, cancellation)
+SELECT
+    order_id,
+    runner_id,
+    CASE WHEN pickup_time IN ('', 'null', NULL) THEN
+        NULL
+    ELSE
+        pickup_time::TIMESTAMP
+    END AS pickup_time2,
+    CASE WHEN distance IN ('', 'null', NULL) THEN
+        NULL
+    ELSE
+        TRIM(REPLACE(distance, 'km', ''))::FLOAT
+    END AS distance2,
+    CASE WHEN duration IN ('', 'null', NULL) THEN
+        NULL
+    ELSE
+        TRIM(REPLACE(REPLACE(REPLACE(duration, 'minutes', ''), 'minute', ''), 'mins', ''))::INTEGER
+    END AS duration2, CASE WHEN cancellation IN ('', 'null', NULL) THEN
+        NULL
+    ELSE
+        cancellation
+    END AS cancellation FROM runner_orders;
+
+-- Creating pizza_recipes_long table
+DROP TABLE IF EXISTS pizza_recipes_long;
+
+CREATE TABLE pizza_recipes_long (
+    pizza_id INTEGER,
+    topping_id INTEGER
+);
+
+INSERT INTO pizza_recipes_long (pizza_id, topping_id)
+SELECT
+    pizza_id,
+    UNNEST(STRING_TO_ARRAY(toppings, ','))::INTEGER AS topping_id
+FROM
+    pizza_runner.pizza_recipes;
